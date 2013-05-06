53f0375720e54f0b2ee6ffa1ff2d4ab79ee41deb
Corey Minyard <cminyard@mvista.com>
2013-05-06 15:36:11 -0500

    Live application coredump
    Add the ability to take a coredump of an application while it is running
    with minimal impact on the application itself.  This creates a copy of the
    process and coredump the copy.
    
    Signed-off-by: Dmitry Antipov <antipov@mvista.com>
    Signed-off-by: Dale Farnsworth <dfarnsworth@mvista.com>
    Signed-off-by: Andrei Konovalov <akonovalov@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- kernel/exit.c
+++ kernel/exit.c
@@ -70,6 +70,7 @@
 #include <linux/user_events.h>
 #include <linux/uaccess.h>
 #include <linux/pidfs.h>
+#include <linux/livedump.h>
 
 #include <uapi/linux/wait.h>
 
@@ -166,6 +167,8 @@ static void __exit_signal(struct release_task_post *post, struct task_struct *ts
 	sighand = rcu_dereference_check(tsk->sighand,
 					lockdep_tasklist_lock_is_held());
 	spin_lock(&sighand->siglock);
+
+	livedump_handle_exit(tsk);
 
 #ifdef CONFIG_POSIX_TIMERS
 	posix_cpu_timers_exit(tsk);
@@ -289,7 +292,8 @@ repeat:
 
 	write_unlock_irq(&tasklist_lock);
 	/* @thread_pid can't go away until free_pids() below */
-	proc_flush_pid(thread_pid);
+	if (!livedump_task_is_clone(p))
+		proc_flush_pid(thread_pid);
 	add_device_randomness(&p->se.sum_exec_runtime,
 			      sizeof(p->se.sum_exec_runtime));
 	free_pids(post.pids);
@@ -758,6 +762,9 @@ static void exit_notify(struct task_struct *tsk, int group_dead)
 				!ptrace_reparented(tsk) ?
 			tsk->exit_signal : SIGCHLD;
 		autoreap = do_notify_parent(tsk, sig);
+	} else if (livedump_task_is_clone_child(tsk)) {
+		/* Always autoreap livedumped cloned threads. */
+		autoreap = true;
 	} else if (thread_group_leader(tsk)) {
 		autoreap = thread_group_empty(tsk) &&
 			do_notify_parent(tsk, tsk->exit_signal);
