b4cd0e44490d4e4f1b8d5e35a7951b8523068304
Corey Minyard <cminyard@mvista.com>
2013-05-06 15:36:11 -0500

    Live application coredump
    Add the ability to take a coredump of an application while it is running
    with minimal impact on the application itself.  This creates a copy of the
    process and coredump the copy.
    
    Signed-off-by: Dmitry Antipov <antipov@mvista.com>
    Signed-off-by: Dale Farnsworth <dfarnsworth@mvista.com>
    Signed-off-by: Andrei Konovalov <akonovalov@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/linux/signal.h
+++ include/linux/signal.h
@@ -231,6 +231,7 @@ struct pt_regs;
 extern int next_signal(struct sigpending *pending, sigset_t *mask);
 extern int do_send_sig_info(int sig, struct siginfo *info,
 				struct task_struct *p, bool group);
+extern int specific_send_sig_info(int, struct siginfo *, struct task_struct *);
 extern int group_send_sig_info(int sig, struct siginfo *info, struct task_struct *p);
 extern int __group_send_sig_info(int, struct siginfo *, struct task_struct *);
 extern int do_sigtimedwait(const sigset_t *, siginfo_t *,
