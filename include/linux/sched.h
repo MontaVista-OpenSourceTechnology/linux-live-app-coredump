0aeac8061e555bf07a5d27390c77ac5fefb64ea9
Corey Minyard <cminyard@mvista.com>
2013-05-06 15:36:11 -0500

    Live application coredump
    Add the ability to take a coredump of an application while it is running
    with minimal impact on the application itself.  This creates a copy of the
    process and coredump the copy.
    
    Signed-off-by: Dmitry Antipov <antipov@mvista.com>
    Signed-off-by: Dale Farnsworth <dfarnsworth@mvista.com>
    Signed-off-by: Andrei Konovalov <akonovalov@mvista.com>
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -39,6 +39,9 @@ struct cfs_rq;
 struct fs_struct;
 struct futex_pi_state;
 struct io_context;
+#ifdef CONFIG_LIVEDUMP
+struct livedump_context;
+#endif
 struct mempolicy;
 struct nameidata;
 struct nsproxy;
@@ -1195,6 +1198,10 @@ struct task_struct {
 #ifdef CONFIG_SECURITY
 	/* Used by LSM modules for access restriction: */
 	void				*security;
+#endif
+#ifdef CONFIG_LIVEDUMP
+	struct livedump_context		*livedump;
+	bool				livedump_sigpending;
 #endif
 
 #ifdef CONFIG_GCC_PLUGIN_STACKLEAK
