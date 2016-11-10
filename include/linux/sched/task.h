240be1a395e812e0baf017e6266385b523ce54ad
Corey Minyard <cminyard@mvista.com>
2016-11-10 13:57:27 -0600

    livedump: Make some functions available for livedump
    Livedump needs access to some things, make them available.
    
    Also return an error from do_coredump so livedump can tell if it failed.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/linux/sched/task.h
+++ include/linux/sched/task.h
@@ -82,6 +82,8 @@ extern void exit_files(struct task_struct *);
 extern void exit_itimers(struct signal_struct *);
 
 extern pid_t kernel_clone(struct kernel_clone_args *kargs);
+extern struct task_struct *copy_process(struct pid *pid, int trace, int node,
+					struct kernel_clone_args *args);
 struct task_struct *fork_idle(int);
 struct mm_struct *copy_init_mm(void);
 extern pid_t kernel_thread(int (*fn)(void *), void *arg, unsigned long flags);
