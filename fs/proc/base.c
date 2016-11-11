e9885e414e64845a1af1765e47cecc396fbc9686
Corey Minyard <cminyard@mvista.com>
2016-11-10 20:21:42 -0600

    livedump: Add a proc fs interface
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -92,6 +92,7 @@
 #include <linux/sched/stat.h>
 #include <linux/flex_array.h>
 #include <linux/posix-timers.h>
+#include <linux/livedump.h>
 #ifdef CONFIG_HARDWALL
 #include <asm/hardwall.h>
 #endif
@@ -2834,6 +2835,97 @@ static int proc_pid_personality(struct seq_file *m, struct pid_namespace *ns,
 	return err;
 }
 
+#ifdef CONFIG_LIVEDUMP
+struct livedump_proc_data {
+	char buf[500];
+	int pos;
+};
+
+static int livedump_open(struct inode *inode, struct file *file)
+{
+	struct livedump_proc_data *pdata;
+
+	pdata = kmalloc(sizeof(*pdata), GFP_KERNEL);
+	if (!pdata)
+		return -ENOMEM;
+	pdata->pos = 0;
+	file->private_data = pdata;
+	return 0;
+}
+
+static ssize_t livedump_write(struct file *file, const char __user *buf,
+			      size_t count, loff_t *ppos)
+{
+	struct livedump_proc_data *pdata = file->private_data;
+	struct livedump_param lparam;
+	int ret = 0;
+	char *curr, *next;
+
+	if (pdata->pos == -1) /* Only do this once per open. */
+		return count;
+
+	if (count + pdata->pos > sizeof(pdata->buf))
+		return -EFBIG;
+	if (copy_from_user(pdata->buf + pdata->pos, buf, count))
+		return -EFAULT;
+
+	curr = strchr(pdata->buf, '\n');
+	if (curr) {
+		struct task_struct *tsk = get_proc_task(file_inode(file));
+
+		pdata->pos = -1;
+
+		if (!tsk)
+			return -ESRCH;
+
+		init_livedump_param(&lparam);
+
+		*curr = '\0';
+		next = pdata->buf;
+		curr = strsep(&next, " \t");
+		for (; curr; curr = strsep(&next, " \t")) {
+			char *parm, *val, *end = "";
+
+			if (!*curr)
+				continue;
+			val = curr;
+			parm = strsep(&val, "=");
+			if (strcmp(parm, "sched_prio") == 0) {
+				lparam.sched_nice = simple_strtol(val, &end, 0);
+			} else if (strcmp(parm, "io_prio") == 0) {
+				lparam.io_prio = simple_strtol(val, &end, 0);
+			} else if (strcmp(parm, "oom_adj") == 0) {
+				lparam.oom_adj = simple_strtol(val, &end, 0);
+			} else if (strcmp(parm, "core_limit") == 0) {
+				lparam.core_limit_set = true;
+				if (strcmp(val, "unlimited") == 0)
+					lparam.core_limit = RLIM_INFINITY;
+				else
+					lparam.core_limit =
+						simple_strtoul(val, &end, 0);
+			} else
+				ret = -EINVAL;
+			if (*end)
+				ret = -EINVAL;
+		}
+
+		if (!ret)
+			ret = do_livedump(tsk, &lparam);
+
+		put_task_struct(tsk);
+
+		if (ret < 0)
+			return ret;
+	}
+	return count;
+}
+
+static const struct file_operations proc_livedump_operations = {
+	.open  = livedump_open,
+	.write = livedump_write,
+};
+#endif
+
 /*
  * Thread groups
  */
@@ -2933,6 +3025,9 @@ static const struct pid_entry tgid_base_stuff[] = {
 	REG("timers",	  S_IRUGO, proc_timers_operations),
 #endif
 	REG("timerslack_ns", S_IRUGO|S_IWUGO, proc_pid_set_timerslack_ns_operations),
+#ifdef CONFIG_LIVEDUMP
+	REG("livedump",   S_IWUSR, proc_livedump_operations),
+#endif
 };
 
 static int proc_tgid_base_readdir(struct file *file, struct dir_context *ctx)
